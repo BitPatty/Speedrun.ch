7b0bb8a06c00c2c162c1e9a1e690feb7ad171001
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-30 02:03:39 +0000

    Deploy HelveticSpeedrunners/speedrun.ch to github.com/HelveticSpeedrunners/speedrun.ch.git:gh-pages

---
+++ precache-manifest.d2cc84ad979bad61d6ad4125f45a4378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0125d97af1c15a12ab1e6207b09c866e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "658cdd69acbabe7628fb",
+    "url": "/static/css/main.cc8fba7a.chunk.css"
+  },
+  {
+    "revision": "7c6a09e4ff2448ad7a43",
+    "url": "/static/js/2.d2bce507.chunk.js"
+  },
+  {
+    "revision": "8265a1bb8f11999f50abcc90ec7ea34e",
+    "url": "/static/js/2.d2bce507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658cdd69acbabe7628fb",
+    "url": "/static/js/main.b5b64d47.chunk.js"
+  },
+  {
+    "revision": "5b5e392ce198dea98828",
+    "url": "/static/js/runtime-main.1cc2f2d8.js"
+  }
+]);
